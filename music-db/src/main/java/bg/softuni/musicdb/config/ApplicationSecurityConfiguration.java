c143cb4d8e7cfca4d7fc1871b6dfdc0c74826543
emil aramazov <emil.aramazow@gmail.com>
2022-12-06 17:36:50 +0200

    Added common html elements

--- music-db/src/main/java/bg/softuni/musicdb/config/ApplicationSecurityConfiguration.java
+++ music-db/src/main/java/bg/softuni/musicdb/config/ApplicationSecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class ApplicationSecurityConfiguration {
@@ -37,8 +38,8 @@ public class ApplicationSecurityConfiguration {
                 .and()
                     .formLogin()
                         .loginPage("/users/login").permitAll()
-                        .usernameParameter("username")
-                        .passwordParameter("password")
+                        .usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY)
+                        .passwordParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY)
                     .defaultSuccessUrl("/home")
                     .failureForwardUrl("/users/login-error")
                 .and()
--- music-db/src/main/java/bg/softuni/musicdb/web/UserController.java
+++ music-db/src/main/java/bg/softuni/musicdb/web/UserController.java
@@ -1,8 +1,12 @@
 package bg.softuni.musicdb.web;
 
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/users")
@@ -16,5 +20,20 @@ public class UserController {
     @GetMapping("/register")
     public String register() {
         return "register";
+    }
+
+    @PostMapping("/login-error")
+
+    // we put field name where is the user form where is username (in ModelAttribute)
+    public ModelAndView failedLogin(@ModelAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY) String username) {
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("bad_credentials", true);
+        modelAndView.addObject("username", username);
+
+        // if we have invalid username and password redirect to:
+        modelAndView.setViewName("/login");
+
+        return modelAndView;
     }
 }
