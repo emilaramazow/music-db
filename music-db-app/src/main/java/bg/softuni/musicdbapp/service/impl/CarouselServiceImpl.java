1ab6bbb72c1e5a28cbdb58c99598d9f7a0edd97a
emilaramazow <emil.aramazow@gmail.com>
2022-12-16 22:31:17 +0200

    Implemented of carousel images rotate with scheduler and cron-job

---
+++ music-db-app/src/main/java/bg/softuni/musicdbapp/service/impl/CarouselServiceImpl.java
@@ -0,0 +1,63 @@
+package bg.softuni.musicdbapp.service.impl;
+
+import bg.softuni.musicdbapp.service.CarouselService;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/*
+    This class is Scheduler class for carousel images (change images after a minute) :
+*/
+
+@Service
+public class CarouselServiceImpl implements CarouselService {
+
+    private Logger LOGGER = LoggerFactory.getLogger(CarouselServiceImpl.class);
+
+    private List<String> images = new ArrayList<>();
+
+    public CarouselServiceImpl(@Value("${carousel.images}") List<String> images) {
+        this.images.addAll(images);
+    }
+    // check if we have enough images for the carousel
+    @PostConstruct
+    public void afterInitialize() {
+        if (images.size() < 3) {
+            throw new IllegalArgumentException("Sorry, but you must configure at least 3 images!");
+        }
+    }
+
+    @Override
+    public String firstImage() {
+        return images.get(0);
+    }
+
+    @Override
+    public String secondImage() {
+        return images.get(1);
+    }
+
+    @Override
+    public String thirdImage() {
+        return images.get(2);
+    }
+
+
+    /*
+        CRON JOB for rotate after 1 minute :
+     */
+
+    @Scheduled(cron = "${carousel.refresh-cron}")
+    public void refresh() {
+        LOGGER.info("Shuffling images...");
+        Collections.shuffle(images);
+    }
+
+}
--- music-db-app/src/main/java/bg/softuni/musicdbapp/web/HomeController.java
+++ music-db-app/src/main/java/bg/softuni/musicdbapp/web/HomeController.java
@@ -1,11 +1,19 @@
 package bg.softuni.musicdbapp.web;
 
+import bg.softuni.musicdbapp.service.CarouselService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class HomeController {
+
+    private final CarouselService carouselService;
+
+    public HomeController(CarouselService carouselService) {
+        this.carouselService = carouselService;
+    }
 
     @GetMapping("/")
     public String index() {
@@ -13,7 +21,12 @@ public class HomeController {
     }
 
     @GetMapping("/home")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("firstImg", carouselService.firstImage());
+        model.addAttribute("secondImg", carouselService.secondImage());
+        model.addAttribute("thirdImg", carouselService.thirdImage());
+
+
         return "home";
     }
 }
