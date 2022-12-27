48f5d3a003fc17b0fbc4e876bfb16e6c176812d6
emilaramazow <emil.aramazow@gmail.com>
2022-12-27 17:35:03 +0200

    Added search bar

--- music-db-app/src/main/java/bg/softuni/musicdbapp/web/AlbumRestController.java
+++ music-db-app/src/main/java/bg/softuni/musicdbapp/web/AlbumRestController.java
@@ -1,8 +1,10 @@
 package bg.softuni.musicdbapp.web;
 
+import bg.softuni.musicdbapp.model.entity.AlbumEntity;
 import bg.softuni.musicdbapp.model.view.AlbumViewModel;
 import bg.softuni.musicdbapp.repository.AlbumRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,11 +25,9 @@ public class AlbumRestController {
     }
 
     @GetMapping("/api")
-    public List<AlbumViewModel> findAll() {
-        return albumRepository
-                .findAll()
-                .stream()
-                .map(ae -> modelMapper.map(ae, AlbumViewModel.class))
-                .collect(Collectors.toList());
+    public ResponseEntity<List<AlbumEntity>> findAll() {
+        return ResponseEntity
+                .ok()
+                .body(albumRepository.findAll());
     }
 }
--- music-db-app/src/main/resources/static/js/search-bar.js
+++ music-db-app/src/main/resources/static/js/search-bar.js
@@ -11,20 +11,50 @@ fetch("http://localhost:8080/albums/api")
         }
     })
 
+console.log(allAlbums);
 
-// TODO: add key listener to the search bar
-// TODO: create method that adds an album....
+searchBar.addEventListener('keyup', (e) => {
+    const searchingCharacters = searchBar.value.toLowerCase();
+    let filteredAlbums = allAlbums.filter(album => {
+        return album.name.toLowerCase().includes(searchingCharacters)
+        || album.artistEntity.name.toLowerCase().includes(searchingCharacters)
+    });
+    displayAlbums(filteredAlbums);
+})
 
+const displayAlbums = (albums) => {
+    albumsList.innerHTML = albums
+        .map((a) => {
+            return ` <div class="col-md-3" >
+                <div class="card mb-4 box-shadow">
+                <img src="${a.imageURL}" class="card-img-top" alt="Thumbnail [100%x225]"
+                     data-holder-rendered="true"
+                     style="height: 225px; width: 100%; display: block;">
+                <div class="card-body">
+                    <div class="text-center">
+                        <p class="card-text border-bottom ">Name: ${a.name}</p>
+                        <p class="card-text border-bottom ">Artist: ${a.artist}</p>
+                        <p class="card-text border-bottom ">Genre: ${a.genre}</p>
+                        <p class="card-text border-bottom ">Price: ${a.price}</p>
+                        <p class="card-text border-bottom">Release Date: ${a.releaseDate}</p>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center">
 
+                        <div class="btn-group">
+                            <a href="/albums/details/${a.id}"  type="button" class="btn btn-sm btn-outline-secondary">Details</a>
+                        </div>
+                        <div class="btn-group">
+                            <a href="/albums/delete/${a.id}"  type="button" class="btn btn-sm btn-outline-secondary">Delete</a>
+                        </div>
 
+                    </div>
+                </div>
+            </div>
+            </div>`
+        })
+        .join('');
 
-
-
-
-
-
-
-
+}
 
 
 
