fe59a3ca10289d003507d623b6c73ffb331eab56
emilaramazow <emil.aramazow@gmail.com>
2022-12-27 20:21:57 +0200

    Added statistics

--- music-db-app/src/main/java/bg/softuni/musicdbapp/service/LogService.java
+++ music-db-app/src/main/java/bg/softuni/musicdbapp/service/LogService.java
@@ -1,5 +1,11 @@
 package bg.softuni.musicdbapp.service;
 
+import bg.softuni.musicdbapp.model.service.LogServiceModel;
+
+import java.util.List;
+
 public interface LogService {
     void createLog(String action, Long albumId);
+
+    List<LogServiceModel> findAllLogs();
 }
--- music-db-app/src/main/java/bg/softuni/musicdbapp/service/impl/LogServiceImpl.java
+++ music-db-app/src/main/java/bg/softuni/musicdbapp/service/impl/LogServiceImpl.java
@@ -3,15 +3,19 @@ package bg.softuni.musicdbapp.service.impl;
 import bg.softuni.musicdbapp.model.entity.AlbumEntity;
 import bg.softuni.musicdbapp.model.entity.LogEntity;
 import bg.softuni.musicdbapp.model.entity.UserEntity;
+import bg.softuni.musicdbapp.model.service.LogServiceModel;
 import bg.softuni.musicdbapp.repository.LogRepository;
 import bg.softuni.musicdbapp.service.AlbumService;
 import bg.softuni.musicdbapp.service.LogService;
 import bg.softuni.musicdbapp.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class LogServiceImpl implements LogService {
@@ -19,11 +23,13 @@ public class LogServiceImpl implements LogService {
     private final LogRepository logRepository;
     private final AlbumService albumService;
     private final UserService userService;
+    private final ModelMapper modelMapper;
 
-    public LogServiceImpl(LogRepository logRepository, AlbumService albumService, UserService userService) {
+    public LogServiceImpl(LogRepository logRepository, AlbumService albumService, UserService userService, ModelMapper modelMapper) {
         this.logRepository = logRepository;
         this.albumService = albumService;
         this.userService = userService;
+        this.modelMapper = modelMapper;
     }
 
     @Override
@@ -46,5 +52,22 @@ public class LogServiceImpl implements LogService {
                 .setDateTime(LocalDateTime.now());
 
         logRepository.save(logEntity);
+    }
+
+    @Override
+    public List<LogServiceModel> findAllLogs() {
+        return logRepository
+                .findAll()
+                .stream()
+                .map(logEntity -> {
+                    // map album and user additionally, because we want it to be strings
+                    LogServiceModel logServiceModel = modelMapper
+                            .map(logEntity, LogServiceModel.class);
+                    logServiceModel.setAlbum(logEntity.getAlbumEntity().getName())
+                            .setUser(logEntity.getUserEntity().getUsername());
+
+                    return logServiceModel;
+                })
+                .collect(Collectors.toList());
     }
 }
